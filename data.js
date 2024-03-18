fc092387c5ca1ce3d884dcdc52da13447a8bbec6
Rivansh <rivansh63@gmail.com>
2024-03-18 16:43:22 +0530

    additional UI changes and populating the data

---
+++ data.js
@@ -0,0 +1,81 @@
+const burger = [
+    {
+        name: "Classic Chicken Burger",
+        price: 500,
+        type: "Regular",
+        img: "assets/burger/1.png"
+    },
+    {
+        name: "Spicy Chicken Burger",
+        price: 550,
+        type: "Regular",
+        img: "assets/burger/2.png"
+    },
+    {
+        name: "Cheeseburger",
+        price: 600,
+        type: "Regular",
+        img: "assets/burger/3.png"
+    },
+    {
+        name: "Vegetarian Burger",
+        price: 450,
+        type: "Regular",
+        img: "assets/burger/4.png"
+    },
+];
+
+const fries = [
+    {
+        name: "Classic Fries",
+        price: 200,
+        type: "Regular",
+        img: "assets/fries/1.png"
+    },
+    {
+        name: "Cheese Fries",
+        price: 250,
+        type: "Regular",
+        img: "assets/fries/2.png"
+    },
+    {
+        name: "Spicy Fries",
+        price: 220,
+        type: "Regular",
+        img: "assets/fries/3.png"
+    },
+    {
+        name: "Sweet Potato Fries",
+        price: 300,
+        type: "Regular",
+        img: "assets/fries/4.png"
+    }
+];
+const drinks = [
+    {
+        name: "Cola",
+        price: 150,
+        type: "Regular",
+        img: "assets/drinks/1.png"
+    },
+    {
+        name: "Lemonade",
+        price: 180,
+        type: "Regular",
+        img: "assets/drinks/2.png"
+    },
+    {
+        name: "Iced Tea",
+        price: 170,
+        type: "Regular",
+        img: "assets/drinks/3.png"
+    },
+    {
+        name: "Milkshake",
+        price: 250,
+        type: "Regular",
+        img: "assets/drinks/4.png"
+    }
+];
+
+
---
+++ script.js
@@ -0,0 +1,71 @@
+const burgerContainer = document.querySelector(".burger-container");
+const friesContainer = document.querySelector(".fries-container"); 
+const drinksContainer = document.querySelector(".drinks-container"); 
+
+function populateData(item, container) {
+    const itemDiv = document.createElement("div");
+    itemDiv.className = "w-[20%] h-[80%] m-2 flex flex-col items-center bg-rose-50 rounded-xl gap-4";
+
+    const imgWrapper = document.createElement("div"); 
+    imgWrapper.className = "w-12 h-12 rounded-full bg-rose-900 flex items-center justify-center"; 
+
+    const img = document.createElement("img");
+    img.src = item.img;
+    img.className = 'w-14 h-14 rounded-full object-cover'; 
+    imgWrapper.appendChild(img); 
+    itemDiv.appendChild(imgWrapper); 
+
+    const name = document.createElement("h1");
+    name.className = "text-xl font-semibold text-gray-600";
+    name.textContent = item.name; 
+    itemDiv.appendChild(name);
+
+    const priceContainer = document.createElement("div");
+    priceContainer.className = "flex gap-2 items-center w-[50%] justify-between";
+
+    const price = document.createElement("h1");
+    price.className = "text-xl font-semibold text-green-400";
+    price.textContent = "$" + item.price; 
+    priceContainer.appendChild(price);
+
+    const type = document.createElement("h2");
+    type.className = "text-l font-semibold text-gray-600";
+    type.textContent = item.type; 
+    priceContainer.appendChild(type);
+    
+    itemDiv.appendChild(priceContainer);
+
+    const buttonDiv = document.createElement('div');
+    buttonDiv.className = 'flex gap-2 items-center w-[70%] justify-between';
+
+    const orderButton = document.createElement("button");
+    orderButton.type = "button";
+    orderButton.className = "w-24 text-black bg-rose-200  hover:bg-rose-300 hover:text-white rounded-lg hover:shadow-xl rounded-xl";
+    orderButton.textContent = "Order";
+
+    buttonDiv.appendChild(orderButton);
+
+    const cancelButton = document.createElement("button");
+    cancelButton.type = "button";
+    cancelButton.className = "w-24 text-black bg-gray-200 hover:bg-gray-100 hover:text-black rounded-lg hover:shadow-xl rounded-xl";
+    cancelButton.textContent = "Cancel";
+    buttonDiv.appendChild(cancelButton);
+
+    itemDiv.appendChild(buttonDiv);
+
+    container.appendChild(itemDiv);
+}
+burger.forEach(burgerItem => {
+    populateData(burgerItem, burgerContainer);
+});
+fries.forEach(friesItem => {
+    populateData(friesItem, friesContainer);
+});
+drinks.forEach(drinkItem => {
+    populateData(drinkItem, drinksContainer);
+});
+
+document.getElementById("viewMenu").addEventListener("click", function() {
+    const burgerSection = document.getElementById("burger-container");
+    burgerSection.scrollIntoView({ behavior: 'smooth' });
+});
